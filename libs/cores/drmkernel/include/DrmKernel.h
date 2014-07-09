6b6d2ee2690e2c34bbc95bae0912af968cbb8bed
Rémi Cohen-Scali <remi.cohen-scali@laposte.net>
2014-07-09 09:04:02 +0200

    Added DRM side of C++ to C wrapper: Definitions of functions & structs

--- libs/cores/drmkernel/include/DrmKernel.h
+++ libs/cores/drmkernel/include/DrmKernel.h
@@ -21,6 +21,97 @@
 extern "C" {
 #endif
 
+  struct NV_DrmMetadata_st {
+    struct NV_DrmMetadata_st *next;
+    char *key;
+    char *value;
+  };
+
+  struct NV_DrmConstraints_st {
+    struct NV_DrmConstraints_st *next;
+    char *key;
+    char *value;
+  };
+
+  struct NV_DrmBuffer_st {
+    char *data;
+    int length;
+  };
+
+  struct NV_DrmInfoAttribute {
+    struct NV_DrmInfoAttribute *next;
+    char *name;
+    char *value;
+  };
+
+  struct NV_DrmInfo_st {
+    int infoType;
+    struct NV_DrmBuffer_st *drmBuffer;
+    char *mimeType;
+    struct NV_DrmInfoAttribute *attributes;
+  };
+
+  struct NV_DrmInfoStatus_st {
+    int statusCode;
+    int infoType;
+    struct NV_DrmBuffer_st *drmBuffer;
+    char *mimeType;
+  };
+
+  struct NV_DrmInfoEvent_st {
+    int uniqueId;
+    int infoType;
+    char *message;
+  };
+
+  struct NV_DrmSupportInfo_mimeTypeNode_st {
+    char *mimeType;
+    struct NV_DrmSupportInfo_mimeTypeNode_st *next;
+  };
+
+  struct NV_DrmSupportInfo_fileSuffixNode_st {
+    char *fileSuffix;
+    struct NV_DrmSupportInfo_mimeTypeNode_st *next;
+  };
+
+  struct NV_DrmSupportInfo_st {
+    struct NV_DrmSupportInfo_mimeTypeNode_st *mimeTypes;
+    struct NV_DrmSupportInfo_fileSuffixNode_st *fileSuffixes;
+    char *description;
+  };
+
+  struct NV_DrmRights_st {
+    struct NV_DrmBuffer_st *data;
+    char *mimeType;
+    char *accountId;
+    char *subscriptionId;
+    char *rightsFromFile;
+  };
+
+  struct NV_DrmRequestInfoMapNode_st {
+    struct NV_DrmRequestInfoMapNode_st *next;
+    char *key;
+    char *value;
+  }
+
+  struct NV_DrmInfoRequest_st {
+    int infoType;
+    char *mimeType;
+    struct NV_DrmRequestInfoMapNode_st  *requestInformationMap;
+  };
+
+  typedef void (NV_OnInfoListener_OnInfoPtr *)(struct NV_DrmInfoEvent_st *event);
+
+  struct NV_DrmMetadata_st* DrmKernel_NvDrmPlugin_onGetMetadata(int uniqueId, const char *path);
+  struct NV_DrmConstraints_st* DrmKernel_NvDrmPlugin_onGetConstraints(int uniqueId, const char *path, int action);
+  struct NV_DrmInfoStatus_st* DrmKernel_NvDrmPlugin_onProcessDrmInfo(int uniqueId, const struct NV_DrmInfo_st *drmInfo);
+  int DrmKernel_NvDrmPlugin_onSetOnInfoListener(int uniqueId, NV_OnInfoListener_OnInfoPtr *lsnr);
+  int DrmKernel_NvDrmPlugin_onInitialize(int uniqueId);
+  int DrmKernel_NvDrmPlugin_onTerminate(int uniqueId);
+  struct NV_DrmSupportInfo_st* DrmKernel_NvDrmPlugin_onGetSupportInfo(int uniqueId);
+  int DrmKernel_NvDrmPlugin_onSaveRights(int uniqueId, struct NV_DrmRights_st *drmRights, const char *rightsPath, const char *contentPath);
+  struct NV_DrmInfo_st* DrmKernel_NvDrmPlugin_onAcquireDrmInfo(int uniqueId, struct NV_DrmInfoRequest_st *drmInfoRequest);
+
 #ifdef __cplusplus
 }
 #endif
